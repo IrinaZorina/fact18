1df810ca08c8c803fe4b4dd869ffb3c7b9f5126f
Alexandr Toropov <toropovsite@yandex.ru>
2024-10-02 20:17:46 +0300

    Add common footer file

---
+++ common/footer.php
@@ -0,0 +1,5 @@
+<footer class="wrapper main_footer border">
+    2024 &copy; Сделано с <span class="heart">&#10084;</span> для <a href="https://academy.fact.digital/" target="_blank">Факт.Академия</a>
+</footer>
+</body>
+</html>
