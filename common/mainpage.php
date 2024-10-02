3fee3e48895e36eb3a6812afef9883b55b3a2a92
Alexandr Toropov <toropovsite@yandex.ru>
2024-10-02 20:18:26 +0300

    Add index page content

---
+++ common/mainpage.php
@@ -0,0 +1,62 @@
+<section class="wrapper main_block border">
+    <aside>
+        <img class="my_photo" src="assets/images/my_photo.jpg" alt="Моя Фотография">
+    </aside>
+    <main>
+        <h1 class="name">Александр Торопов</h1>
+        <div class="content">
+            <div class="content_item">
+                <p>Пишу разный <a href="https://github.com/aletoropov" target="_blank" title="Мой GitHub">код</a> на PHP и JavaScript, использую Linux Mint, делаю буклеты в Abode InDesign.</p>
+                <p>Люблю уехать в закат на велосипеде &#128690;</p>
+                <p>Люблю создавать цифровые продукты, которые приносят пользу людям &#128187;</p>
+                <p>Могу управлять скоростным поездом &#128646;</p>
+            </div>
+            <div class="content_item">
+                <p>Все понравилось, думаю это будет увлекательное приключение в мир кодинга и 1C Bitrix.</p>
+                <p>Надеюсь успешно справиться со всеми испытаниями и стать сертифицированным 1C Bitrix разработчиком.</p>
+            </div>
+        </div>
+    </main>
+</section>
+<section class="wrapper main_block border">
+    <h2 class="section_header">Достопримечательности Красноярского края</h2>
+    <ul class="attractions_small">
+        <li class="attractions_item">
+            <img class="attractions_image" src="assets/images/attractions1.jpg" alt="Бобровый лог">
+            <h3>Бобровый лог</h3>
+        </li>
+        <li class="attractions_item">
+            <img class="attractions_image" src="assets/images/attr_small1.jpg" alt="Пещера Большая Орешная">
+            <h3>Пещера Большая Орешная</h3>
+        </li>
+        <li class="attractions_item">
+            <img class="attractions_image" src="assets/images/attr_small2.jpg" alt="Национальный парк Столбы">
+            <h3>Национальный парк "Столбы"</h3>
+        </li>
+        <li class="attractions_item">
+            <img class="attractions_image" src="assets/images/attr_small3.jpg" alt="ГЭС">
+            <h3>ГЭС</h3>
+        </li>
+    </ul>
+</section>
+<section class="wrapper main_block border">
+    <h2 class="section_header">Достопримечательности города Красноярска</h2>
+    <ul class="attractions_big">
+        <li class="attractions_item">
+            <img class="attractions_image" src="assets/images/attractions1.jpg" alt="Красивый мост">
+            <h3>Часовня</h3>
+        </li>
+        <li class="attractions_item">
+            <img class="attractions_image" src="assets/images/attractions2.jpg" alt="Дои и река">
+            <h3>Река</h3>
+        </li>
+        <li class="attractions_item">
+            <img class="attractions_image" src="assets/images/attractions2.jpg" alt="Дои и река">
+            <h3>Дома</h3>
+        </li>
+        <li class="attractions_item">
+            <img class="attractions_image" src="assets/images/attractions1.jpg" alt="Красивый мост">
+            <h3>Набережная</h3>
+        </li>
+    </ul>
+</section>
