7e25b842fffb223c8675e05a20dcdf7aad9b686f
Alexandr Toropov <toropovsite@yandex.ru>
2024-10-02 22:09:01 +0300

    Add social link

--- common/footer.php
+++ common/footer.php
@@ -1,5 +1,18 @@
 <footer class="wrapper main_footer border">
-    2024 &copy; Сделано с <span class="heart">&#10084;</span> для <a href="https://academy.fact.digital/" target="_blank">Факт.Академия</a>
+    <div>
+        2024 &copy; Сделано с <span class="heart">&#10084;</span> для <a href="https://academy.fact.digital/" target="_blank">Факт.Академия</a>
+    </div>
+    <div>
+        <a class="icon_footer" href="https://vk.com/aletorop" target="_blank" title="Страница ВКонтакте">
+            <img src="/assets/images/vk.svg">
+        </a>
+        <a class="icon_footer" href="#" target="_blank" title="Написать в Telegram">
+            <img src="/assets/images/telegram.svg">
+        </a>
+        <a class="icon_footer" href="mailto:toropovsite@yandex.ru" target="_blank"  title="Написать на e-mail">
+            <img src="/assets/images/email.png">
+        </a>
+    </div>
 </footer>
 </body>
 </html>
