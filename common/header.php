49c46df769c090f65cc6b401079efceeb4954c50
Alexandr <toropovsite@yandex.ru>
2024-10-03 13:55:59 +0300

    fix: merge

--- common/header.php
+++ common/header.php
@@ -16,7 +16,6 @@ if ($hour > 8 && $hour < 22) {
     <meta name="description" content="Страница для курса по 1C-Битрикс">
     <title>Александр Торопов</title>
     <link rel="stylesheet" href="<?=$style;?>">
-<<<<<<< Updated upstream
     <link rel="stylesheet" href="assets/css/mendeleev.css">
 </head>
 <body>
@@ -29,15 +28,3 @@ if ($hour > 8 && $hour < 22) {
         <a class="nav_item" href="loop.php">Циклы</a>
     </nav>
 </header>
-=======
-</head>
-<body>
-<header class="wrapper main_header border">
-    <a href="index.html">
-        <img src="assets/images/logo.svg" alt="Логотип Факт.Академия">
-    </a>
-    <nav>
-        <a href="mendeleev.php">Таблица Менделеева</a>
-    </nav>
-</header>
->>>>>>> Stashed changes
