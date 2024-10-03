ded0d2572bec6a7dfe51cf6f9722437fc27daa70
Александр Торопов <toropovsite@yandex.ru>
2024-10-03 14:26:55 +0300

    Add alt text for images

--- common/footer.php
+++ common/footer.php
@@ -4,13 +4,13 @@
     </div>
     <div>
         <a class="icon_footer" href="https://vk.com/aletorop" target="_blank" title="Страница ВКонтакте">
-            <img src="/assets/images/vk.svg">
+            <img src="/assets/images/vk.svg" alt="Вконтакте">
         </a>
         <a class="icon_footer" href="https://t.me/alesite" target="_blank" title="Написать в Telegram">
-            <img src="/assets/images/telegram.svg">
+            <img src="/assets/images/telegram.svg" alt="Телеграм">
         </a>
         <a class="icon_footer" href="mailto:toropovsite@yandex.ru" target="_blank"  title="Написать на e-mail">
-            <img src="/assets/images/email.png">
+            <img src="/assets/images/email.png" alt="E-mail">
         </a>
     </div>
 </footer>
