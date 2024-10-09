d2d3cbfc5e8bf018496f6f006cbfc1a6fedafc65
Александр Торопов <toropovsite@yandex.ru>
2024-10-09 17:30:13 +0300

    Add page for string homework

---
+++ common/string.php
@@ -0,0 +1,18 @@
+<section class="wrapper main_block border">
+    <main>
+        <h1 class="name">Домашняя работа "Строки"</h1>
+        <div class="content">
+            <div class="content_item">
+                <p>Задача № слайд </p>
+                <p><!-- TODO: текст задачи --></p>
+            </div>
+            <div class="content_item">
+                <p>
+                    <?php
+                    // TODO: решение
+                    ?>
+                </p>
+            </div>
+        </div>
+    </main>
+</section>
---
+++ string.php
@@ -0,0 +1,7 @@
+<?php
+
+include_once __DIR__ . '/common/header.php';
+
+include_once __DIR__ . '/common/string.php';
+
+include_once __DIR__ . '/common/footer.php';
