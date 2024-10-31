4430b9531cf702678258cfcb3b32faa4eb4d1695
Kirill Sitnikov <2-akkaynt@mail.ru>
2024-10-31 16:44:45 +0300

    commit1

--- header.php
+++ header.php
@@ -25,19 +25,19 @@
         <nav>
             <div class="navbar1">
                 <ul>
-                    <li><a href="index.php"> О компании</a> </li>
-                    <li><a href="cycles.php">Циклы</a> </li>
-                    <li><a href="arrays.php">Массивы</a> </li>
-                    <li><a href="string.php">Строки</a> </li>
-                    <li><a href="functions.php">Функции</a> </li>
-                    <li><a href="getpost.php">Get и Post</a> </li>
-                    <li><a href="cookiesession.php">COOKIE и SESSION</a> </li>
+                    <a href="index.php"> О компании</a>
+                    <a href="cycles.php">Циклы</a>
+                    <a href="arrays.php">Массивы</a>
+                    <a href="string.php">Строки</a>
+                    <a href="functions.php">Функции</a>
+                    <a href="getpost.php">Get и Post</a>
+                    <a href="cookiesession.php">COOKIE и SESSION</a>
                 </ul>
                 <ul>
-                    <li><a href="#">VK</a> </li>
-                    <li><a href="#">Google</a> </li>
-                    <li><a href="#">Twitter</a> </li>
-                    <li><a href="#">Telegram</a> </li>
+                    <a href="#">VK</a>
+                    <a href="#">Google</a>
+                    <a href="#">Twitter</a>
+                    <a href="#">Telegram</a> 
                 </ul>
             </div>
         </nav>
