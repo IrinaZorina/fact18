73ae3c133cfa40e42cee57b718338acf5659945d
Александр Торопов <toropovsite@yandex.ru>
2024-11-04 10:29:46 +0300

    Database dump

---
+++ fact18.sql
@@ -0,0 +1,11 @@
+-- --------------------------------------------------------
+-- Хост:                         127.127.126.26
+-- Версия сервера:               8.0.35 - MySQL Community Server - GPL
+-- Операционная система:         Win64
+-- HeidiSQL Версия:              12.8.0.6951
+-- --------------------------------------------------------
+
+-- Дамп данных таблицы fact18.users: ~1 rows (приблизительно)
+DELETE FROM `users`;
+INSERT INTO `users` (`id`, `login`, `password`) VALUES
+	(1, 'admin', '$2y$10$RCF0FQULn3RyjwAUygdi5uFp6SqUwOoHfjNrXp83tfNXz8cRjt622');
